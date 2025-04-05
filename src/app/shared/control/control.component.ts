f6187750085af78413448cc86a17b6d13ecd4787
orneladodaj <orneladodaj.work@gmail.com>
2025-04-05 18:07:32 +0200

    scooping css styles

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,12 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class ControlComponent {
   label = input.required<string>();
