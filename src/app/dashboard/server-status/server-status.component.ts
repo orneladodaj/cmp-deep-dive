0a5fe12f7cb1d84b86ab9abade66af244f29ae7d
orneladodaj <orneladodaj.work@gmail.com>
2025-04-08 22:20:37 +0200

    used template variables

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,12 +7,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown'= 'online';
+  private interval?: ReturnType<typeof setInterval>;
   constructor(){}
   ngOnInit(){
-    setInterval( () => {
+     this.interval = setInterval( () => {
       const rnd = Math.random();
+
       if(rnd < 0.5) {
         this.currentStatus = 'online'
       } else if (rnd < 0.9) {
@@ -21,5 +23,8 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     },5000);
+  }
+  ngOnDestroy(){
+    clearTimeout(this.interval);
   }
 }
--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent],
+  imports: [ControlComponent,ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-
+onSubmit(title: string, ticketTest: String){
+  console.log(title);
+  console.log(ticketTest);
+}
 }
